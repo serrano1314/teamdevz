b491788d0704d090e33ba24f1fa134a78c332e70
Steven Serrano <serrano1314@gmail.com>
2022-01-27 00:22:06 +0800

    add loop for profiles

--- developers/dev-info.js
+++ developers/dev-info.js
@@ -15,24 +15,27 @@ let dev_profile = [
         id: '0',
         first_name: "Steven",
         last_name: "Serrano",
+        skills: ['Frontend Webdev','Adobe Photoshop','Adobe Premiere'],
         bio: "paro paro g, fly high butterfly",
         contact_no: '0926079xxxx',
         email: 'steven.serrano@tup.edu.ph'
     },
     {
         id: '1',
-        first_name: "",
-        last_name: "",
-        bio: "",
-        contact_no: '',
-        email: ''
+        first_name: "Briane Gail",
+        last_name: "Castada",
+        skills: ['skill1','skill2','skill3'],
+        bio: "paro paro g, fly high butterfly 2",
+        contact_no: '09672803133',
+        email: 'Brianegailcastada@Tup.Edu.Ph'
     },
     {
         id: '2', //please add 1 if adding new profile
-        first_name: "",
-        last_name: "",
-        bio: "",
-        contact_no: '',
-        email: ''
+        first_name: "Raissel",
+        last_name: "Lamangan",
+        skills: ['skill1','skill2','skill3'],
+        bio: "paro paro g, fly high butterfly 2",
+        contact_no: '0916-772-9926',
+        email: 'Raissel.Lamangan@Tup.Edu.Ph'
     }
         ];
