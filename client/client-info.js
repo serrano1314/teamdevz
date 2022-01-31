5e7157b885b7b9491657d730334a0f1cfb0fdc9a
Steven Serrano <serrano1314@gmail.com>
2022-01-31 22:00:52 +0800

    add initial code for client side

---
+++ client/client-info.js
@@ -0,0 +1,65 @@
+let jobs = [
+    {
+        id: '0',
+        employer_name: 'Ansh Chauhan',
+        job_name: 'eCommerce Website Customisations (Shopify)',
+        job_description: `I am looking for an expert to work with on a regular basis to make some 
+                            customisations/development to our current Shopify Theme to enhance our brand and customer experience. 
+                            Listed below are some customisations I'd like to begin implementing:`,
+        type: 'Hourly',
+        budget: 972.42
+    },
+    {
+        id: '1',
+        employer_name: 'Prem Atwal',
+        job_name: 'Full SEO Audit of an Affiliate Content Site',
+        job_description: `I'm looking for an experienced SEO auditor to run an audit on my affiliate content site in the music niche.
+        I'm currently backed up with lots of other projects, so I would prefer to outsource this task. The audit should cover everything and include a checklist of things that need to be implemented.
+        The candidate should have experience of performing an SEO audit and devising a checklist.
+        History of previous SEO audits should be provided along with a strong rating.`,
+        type: 'Hourly',
+        budget: 900
+    },
+    {
+        id: '2',
+        employer_name: 'Mayur Deshpande',
+        job_name: 'Node JS e-commerce api for multivendor platform',
+        job_description: `Writing node JS , expresses mongodb api for order , multi order item for multivendor, multi user and admin.
+
+        Order need status which confirmation from vendor, shipped out provided with shipping tracking number, order completed.
+        
+        Order will have item imagecover,
+        Item price, item quantity total price per order.
+        
+        Order will show shipping status until the item is delivered
+        
+        If user purchase multi items in the order, the items is from same vendor and  items purchase will be listed in an order.
+        
+        If user purchase multi item form different vendor it will generate separate order.
+        
+        
+        
+        Affiliate.Js
+        
+        Need affiliate js which blogger user can generate the affiliate link on their side based on our product.
+        They can generate income if user buy our product on our website. Their income is on commissions is based on the sales they generate from affiliate link.
+        
+        Point js
+        Which user can gain point after they have complete their purchase
+        How many Dollar expense can gain 1 point
+        The point will expiry after one year unused and they have the right to used the point as a reduction in the payment
+        How many point is equivalent to 1 dollar`,
+        type: 'One-time Project',
+        budget: 500
+    },
+    {
+        id: '3',
+        employer_name: '',
+        job_name: 'Email Marketing',
+        job_description: `We are looking for a crack email marketer, to manage our email marketing for our e-commerce site.
+
+        Please tell me your experience in email marketing`,
+        type: 'Hourly',
+        budget: 1000
+    }
+];
